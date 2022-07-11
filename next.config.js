cfd52e9970cfdd505e81332c1d0a17cb921003be
kuko <kuko22@pm.me>
2022-07-11 10:15:48 +0900

    Remove babel plugin
    Use swc instead of it.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      fileName: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
